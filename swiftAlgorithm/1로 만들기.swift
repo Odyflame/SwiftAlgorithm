5263e14d7738ae38f2d78ed47eb06959c856c16b
hyeonjae Lee <odyflame@icloud.com>
2020-05-26 20:28:47 +0900

    add swift practice

---
+++ swiftAlgorithm/1로 만들기.swift
@@ -0,0 +1,9 @@
+//
+//  1로 만들기.swift
+//  swiftAlgorithm
+//
+//  Created by apple on 2020/05/26.
+//  Copyright © 2020 odyflame. All rights reserved.
+//
+
+import Foundation
---
+++ swiftAlgorithm/A + B - 7.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  swiftAlgorithm
+//
+//  Created by apple on 2020/05/26.
+//  Copyright © 2020 odyflame. All rights reserved.
+//
+
+import Foundation
+
+var arr = Int(readLine()!)!
+
+for i in 0 ..< arr {
+    let arr2 = readLine()
+    let arr3 = arr2!.components(separatedBy: " ")
+    
+    print("Case #\(i + 1): \(Int(arr3[0])! + Int(arr3[1])!)")
+}
