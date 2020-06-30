b4817e4644d3f422cbc6cd946502bce32ddc691e
hyeonjae Lee <odyflame@icloud.com>
2020-06-30 18:08:21 +0900

    add algo solve

--- swiftAlgorithm/swiftProgrammers/프린터.swift
+++ swiftAlgorithm/swiftProgrammers/프린터.swift
@@ -8,37 +8,37 @@
 
 import Foundation
 
-func solution(_ priorities:[Int], _ location:Int) -> Int {
-     
-    var answer = [(Int, Int)]()
-    var sum = 0
-    
-    var index = 0
-    for i in priorities {
-         answer.append( (i, index) )
-        index += 1
-    }
-    
-    while !answer.isEmpty {
-        var temp = answer.removeFirst()
-        var flag = false
-        for i in answer {
-            if i.0 > temp.0 {
-                flag = true
-                break
-            }
-        }
-        
-        if flag {
-            answer.append(temp)
-            continue
-        } else {
-            sum += 1
-            if temp.1 == location {
-                break
-            }
-        }
-    }
-    
-    return sum
-}
+//func solution(_ priorities:[Int], _ location:Int) -> Int {
+//     
+//    var answer = [(Int, Int)]()
+//    var sum = 0
+//    
+//    var index = 0
+//    for i in priorities {
+//         answer.append( (i, index) )
+//        index += 1
+//    }
+//    
+//    while !answer.isEmpty {
+//        var temp = answer.removeFirst()
+//        var flag = false
+//        for i in answer {
+//            if i.0 > temp.0 {
+//                flag = true
+//                break
+//            }
+//        }
+//        
+//        if flag {
+//            answer.append(temp)
+//            continue
+//        } else {
+//            sum += 1
+//            if temp.1 == location {
+//                break
+//            }
+//        }
+//    }
+//    
+//    return sum
+//}
