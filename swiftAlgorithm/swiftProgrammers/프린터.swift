a689e6cdeb3aec0dcb47edae5b3ce58c7d51cf0e
hyeonjae Lee <odyflame@icloud.com>
2020-06-30 17:12:03 +0900

    add algo solve

---
+++ swiftAlgorithm/swiftProgrammers/프린터.swift
@@ -0,0 +1,44 @@
+//
+//  프린터.swift
+//  swiftAlgorithm
+//
+//  Created by apple on 2020/06/30.
+//  Copyright © 2020 odyflame. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ priorities:[Int], _ location:Int) -> Int {
+     
+    var answer = [(Int, Int)]()
+    var sum = 0
+    
+    var index = 0
+    for i in priorities {
+         answer.append( (i, index) )
+        index += 1
+    }
+    
+    while !answer.isEmpty {
+        var temp = answer.removeFirst()
+        var flag = false
+        for i in answer {
+            if i.0 > temp.0 {
+                flag = true
+                break
+            }
+        }
+        
+        if flag {
+            answer.append(temp)
+            continue
+        } else {
+            sum += 1
+            if temp.1 == location {
+                break
+            }
+        }
+    }
+    
+    return sum
+}
