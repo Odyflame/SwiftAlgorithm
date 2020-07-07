68e2553b132dec58740ea2f7623a5c1169fda7f0
hyeonjae Lee <odyflame@icloud.com>
2020-07-07 19:43:49 +0900

    add algo solve

---
+++ swiftAlgorithm/swiftProgrammers/Level2/오픈채팅방.swift
@@ -0,0 +1,35 @@
+//
+//  오픈채팅방.swift
+//  swiftAlgorithm
+//
+//  Created by apple on 2020/07/07.
+//  Copyright © 2020 odyflame. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ record:[String]) -> [String] {
+    
+    var myDic = [String: String]()
+    var answer = [String]()
+    
+    for srr in record {
+        let order = srr.components(separatedBy: " ")
+        if order[0] == "Enter" {
+            myDic[order[1]] = order[2]
+        } else if order[0] == "Change" {
+            myDic[order[1]] = order[2]
+        }
+    }
+    
+    for srr in record {
+        let order = srr.components(separatedBy: " ")
+        if order[0] == "Enter" {
+            answer.append( "\(myDic[order[1]]!)님이 들어왔습니다." )
+        } else if order[0] == "Leave" {
+            answer.append( "\(myDic[order[1]]!)님이 나갔습니다." )
+        }
+    }
+    
+    return answer
+}
---
+++ swiftAlgorithm/swiftProgrammers/Level2/프렌즈4블록.swift
@@ -0,0 +1,10 @@
+//
+//  프렌즈4블록.swift
+//  swiftAlgorithm
+//
+//  Created by apple on 2020/07/07.
+//  Copyright © 2020 odyflame. All rights reserved.
+//
+
+import Foundation
+
