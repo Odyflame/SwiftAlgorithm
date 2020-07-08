aa94046b38922e3003a3ed74deb3699805e78ece
hyeonjae Lee <odyflame@icloud.com>
2020-07-08 14:03:09 +0900

    add algo solve

---
+++ swiftAlgorithm/swiftProgrammers/Level3/가장 긴 팰린드롬.swift
@@ -0,0 +1,58 @@
+//
+//  가장 긴 팰린드롬.swift
+//  swiftAlgorithm
+//
+//  Created by apple on 2020/07/08.
+//  Copyright © 2020 odyflame. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ s:String) -> Int {
+    var answer = 1
+    
+    func checkPalindrom(_ stratIndex: Int, _ endIndex: Int) -> Bool {
+        var sArray = Array(s)
+        var startIndex = stratIndex
+        var endIndex = endIndex
+        
+        while startIndex < endIndex {
+            if sArray[startIndex] != sArray[endIndex] {
+                return false
+            }
+            startIndex += 1
+            endIndex -= 1
+        }
+        return true
+    }
+
+    if s.count <= 1 {
+        return answer
+    }
+    var sArray = Array(s)
+    for i in 0 ..< s.count {
+        for j in stride(from: s.count - 1, to: i, by: -1) {
+            
+            var startIndex = i
+            var endIndex = j
+            var flag = true
+            
+            while startIndex < endIndex {
+                if sArray[startIndex] != sArray[endIndex] {
+                    flag = false
+                    break
+                }
+                startIndex += 1
+                endIndex -= 1
+            }
+            
+            if flag {
+                answer = max(answer, j - i + 1)
+                break
+            }
+            
+        }
+    }
+    
+    return answer
+}
--- swiftAlgorithm/swiftProgrammers/Level3/방문 길이.swift
+++ swiftAlgorithm/swiftProgrammers/Level3/방문 길이.swift
@@ -96,53 +96,53 @@ import Foundation
 //    return walkedRoads.count
 //}
 
-struct point: Hashable {
-    // 구조체일 경우에는 모두 Hashable을 준수해야 한다. 열거형일 경우 모든 associated valeus는 모두 Hashable을 준수해야 한다.
-    // Hashable의 정의는 정수 Hash값을 제공하는 타입이다. 이 정수 Hash가 있기 때문에 우리가 찾으려는 원소를 빨리 찾을 수 있다.
-    var x: Int
-    var y: Int
-    
-    func move(_ to: Character) -> point? {
-        switch to {
-        case "U":
-            guard self.y < 5 else { return nil }
-            return point(x: self.x, y: self.y + 1)
-        case "D":
-            guard self.y > -5 else { return nil }
-            return point(x: self.x, y: self.y - 1)
-        case "R" :
-            guard self.x < 5 else { return nil }
-            return point(x: self.x + 1, y: self.y)
-            case "L" :
-            guard self.x > -5 else { return nil }
-            return point(x: self.x - 1, y: self.y)
-        default:
-            return nil
-        }
-    }
-}
-
-struct path: Hashable {
-    var startPoint: point
-    var endPoint: point
-}
-
-func solution(_ dirs:String) -> Int {
-    
-    var pos = point(x: 0, y: 0)
-    var paths = Set<path>()
-    
-    dirs.forEach {
-        guard let next = pos.move( $0 ) else {
-            return
-        }
-        
-        paths.insert(path(startPoint: pos, endPoint: next))
-        paths.insert(path(startPoint: next, endPoint: pos))
-        
-        pos = next
-    }
-    
-    return paths.count / 2
-}
+//struct point: Hashable {
+//    // 구조체일 경우에는 모두 Hashable을 준수해야 한다. 열거형일 경우 모든 associated valeus는 모두 Hashable을 준수해야 한다.
+//    // Hashable의 정의는 정수 Hash값을 제공하는 타입이다. 이 정수 Hash가 있기 때문에 우리가 찾으려는 원소를 빨리 찾을 수 있다.
+//    var x: Int
+//    var y: Int
+//
+//    func move(_ to: Character) -> point? {
+//        switch to {
+//        case "U":
+//            guard self.y < 5 else { return nil }
+//            return point(x: self.x, y: self.y + 1)
+//        case "D":
+//            guard self.y > -5 else { return nil }
+//            return point(x: self.x, y: self.y - 1)
+//        case "R" :
+//            guard self.x < 5 else { return nil }
+//            return point(x: self.x + 1, y: self.y)
+//            case "L" :
+//            guard self.x > -5 else { return nil }
+//            return point(x: self.x - 1, y: self.y)
+//        default:
+//            return nil
+//        }
+//    }
+//}
+//
+//struct path: Hashable {
+//    var startPoint: point
+//    var endPoint: point
+//}
+//
+//func solution(_ dirs:String) -> Int {
+//
+//    var pos = point(x: 0, y: 0)
+//    var paths = Set<path>()
+//
+//    dirs.forEach {
+//        guard let next = pos.move( $0 ) else {
+//            return
+//        }
+//
+//        paths.insert(path(startPoint: pos, endPoint: next))
+//        paths.insert(path(startPoint: next, endPoint: pos))
+//
+//        pos = next
+//    }
+//
+//    return paths.count / 2
+//}
 
