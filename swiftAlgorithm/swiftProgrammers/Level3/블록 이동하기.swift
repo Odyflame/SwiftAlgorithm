15985a7c26d68fea59c64aa1b5802ca9bbc14678
hyeonjae Lee <odyflame@icloud.com>
2020-07-10 21:24:05 +0900

    add algo solve

--- swiftAlgorithm/swiftProgrammers/Level3/블록 이동하기.swift
+++ swiftAlgorithm/swiftProgrammers/Level3/블록 이동하기.swift
@@ -6,44 +6,44 @@
 //  Copyright © 2020 odyflame. All rights reserved.
 //
 
-import Foundation
-
-func solution(_ board:[[Int]]) -> Int {
-    
-    typealias point = (x: Int, y: Int)
-    
-    struct airplane {
-        var leftWing: point
-        var rightWing: point
-    }
-    
-    func checkRange(_ x: Int, _ y: Int) -> Bool {
-        if x >= 0 && x < board.count && y >= 0 && y < board.count {
-            return true
-        }
-        return false
-    }
-    
-    var dl = [ (1, 0 ), (-1, 0 ), (0, 1 ), (0, -1 ), (0, 0 ), (0, 0 ), (-1, 1 ), (1, 1 ) ]
-    var dr = [ (1, 0 ), (-1, 0 ), (0, 1 ), (0, -1 ), (-1, -1 ), (1, -1 ), (0, 0 ), (0, 0 ) ]
-    var dp = Array(repeating: Array(repeating: 9999999, count: 101), count: 101)
-    
-    var qu = [(airplane, Int)]()
-    qu.append( (airplane(leftWing: point(0,0), rightWing: point(0,1)), 0 ) )
-    dp[0][0] = 0
-    
-    while !qu.isEmpty {
-        var temp = qu.removeFirst()
-        
-        for i in 0..<8 {
-            if i < 4 {
-                if checkRange(<#T##x: Int##Int#>, <#T##y: Int##Int#>)
-            } else {
-                
-            }
-        }
-        
-    }
-    
-    return 0
-}
+//import Foundation
+//
+//func solution(_ board:[[Int]]) -> Int {
+//    
+//    typealias point = (x: Int, y: Int)
+//    
+//    struct airplane {
+//        var leftWing: point
+//        var rightWing: point
+//    }
+//    
+//    func checkRange(_ x: Int, _ y: Int) -> Bool {
+//        if x >= 0 && x < board.count && y >= 0 && y < board.count {
+//            return true
+//        }
+//        return false
+//    }
+//    
+//    var dl = [ (1, 0 ), (-1, 0 ), (0, 1 ), (0, -1 ), (0, 0 ), (0, 0 ), (-1, 1 ), (1, 1 ) ]
+//    var dr = [ (1, 0 ), (-1, 0 ), (0, 1 ), (0, -1 ), (-1, -1 ), (1, -1 ), (0, 0 ), (0, 0 ) ]
+//    var dp = Array(repeating: Array(repeating: 9999999, count: 101), count: 101)
+//    
+//    var qu = [(airplane, Int)]()
+//    qu.append( (airplane(leftWing: point(0,0), rightWing: point(0,1)), 0 ) )
+//    dp[0][0] = 0
+//    
+//    while !qu.isEmpty {
+//        var temp = qu.removeFirst()
+//        
+//        for i in 0..<8 {
+//            if i < 4 {
+//                if checkRange(<#T##x: Int##Int#>, <#T##y: Int##Int#>)
+//            } else {
+//                
+//            }
+//        }
+//        
+//    }
+//    
+//    return 0
+//}
---
+++ swiftAlgorithm/swiftProgrammers/Level3/여행경로.swift
@@ -0,0 +1,30 @@
+//
+//  여행경로.swift
+//  swiftAlgorithm
+//
+//  Created by apple on 2020/07/10.
+//  Copyright © 2020 odyflame. All rights reserved.
+//
+
+//import Foundation
+//
+//func solution(_ tickets:[[String]]) -> [String] {
+//    
+//    struct ticket {
+//        var startArea: String
+//        var list = [[String]]()
+//        var answer = [String]()
+//        
+//        static func < ( a: ticket, b: ticket) -> Bool {
+//            return a.startArea < b.startArea
+//        }
+//    }
+//    
+//    var ticketList = [ticket]()
+//    
+//    for i in tickets {
+//        
+//    }
+//    
+//    return []
+//}
