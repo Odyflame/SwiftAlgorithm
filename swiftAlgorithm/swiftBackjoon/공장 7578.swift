934612bf815237fbf54c99e5a85179de8e7f0a0a
hyeonjae Lee <odyflame@icloud.com>
2020-11-14 19:00:10 +0900

    add algo not solve....

---
+++ swiftAlgorithm/swiftBackjoon/공장 7578.swift
@@ -0,0 +1,25 @@
+//
+//  공장 7578.swift
+//  swiftAlgorithm
+//
+//  Created by apple on 2020/11/14.
+//  Copyright © 2020 odyflame. All rights reserved.
+//
+
+import Foundation
+
+func solution7578() {
+    
+    var N = Int(readLine()!)!
+    
+    var arr1 = readLine()!.split(separator: " ").map { Int($0)! }
+    var arr2 = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    var tree = [Int: Int]()
+    
+    for i in 0 ..< arr2.count {
+        
+        
+        
+    }
+}
