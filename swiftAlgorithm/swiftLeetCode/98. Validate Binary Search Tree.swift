03bcdadc70c3660c783c9797fd8c87b4188826df
hyeonjae Lee <odyflame@icloud.com>
2021-06-29 14:24:23 +0900

    add algo solve

---
+++ swiftAlgorithm/swiftLeetCode/98. Validate Binary Search Tree.swift
@@ -0,0 +1,9 @@
+//
+//  98. Validate Binary Search Tree.swift
+//  swiftAlgorithm
+//
+//  Created by apple on 2021/06/29.
+//  Copyright © 2021 odyflame. All rights reserved.
+//
+
+import Foundation
